bf8292f8b28e9dc116a76ac6316725fc19bf2acf
WeeraphatOO <weeraphat.o@ku.th>
2026-02-02 16:36:47 +0700

    treshold for Unet hybrid

--- pages/try your data.py
+++ pages/try your data.py
@@ -376,7 +376,7 @@ with center:
                         seg
                     )
 
-                hybrid_mask = (pred_mask > 0).astype(np.uint8)
+                hybrid_mask = (pred_mask > 0.2).astype(np.uint8)
 
                 # =========================
                 # COLOR OVERLAY (YOLO CLASS)
