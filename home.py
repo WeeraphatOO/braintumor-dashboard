0c689260c3cd45e0f2f47311820968277d9cc7de
WeeraphatOO <weeraphat.o@ku.th>
2026-02-17 08:33:42 +0700

    Add Developed by

--- home.py
+++ home.py
@@ -6,36 +6,48 @@ st.set_page_config(
     layout="wide"
 )
 
-st.title("🏠 Dashboard Home")
+st.title("Dashboard Home")
 
 st.markdown("""
-### 🧠 Brain Tumor Detection and Segmentation – Senior Project
+### Brain Tumor Detection and Segmentation System Using CNNs
 
-Welcome to the **Brain Tumor Detection and Segmentation** senior project dashboard.
+Welcome to the **Brain Tumor Detection and Segmentation** dashboard.
 
-This project focuses on applying **deep learning and computer vision techniques** to analyze brain MRI images for:
+This project focuses on applying **CNNs techniques** to analyze brain MRI images for:
 - **Tumor detection** (localizing tumor regions)
 - **Tumor segmentation** (pixel-level classification of tumor areas)
 - **Multi-class tumor analysis** across different tumor types
 
-#### 🎯 Project Purpose
+#### Project Purpose
 The main objectives of this project are:
-- To develop and compare **multiple deep learning models** for brain tumor analysis  
+- To develop and compare **multiple CNN models** for brain tumor analysis  
 - To evaluate the performance of **segmentation-based and detection-based approaches**
 - To provide clear visualizations and metrics for model evaluation
 
-#### 🧪 Models Implemented
+#### Models Implemented
 - **U-Net–based segmentation models**
 - **YOLO segmentation models**
 - **Hybrid models (YOLO + U-Net)** for region-focused segmentation
 
-#### 📊 What You Can Explore
+#### What You Can Explore
 Using the sidebar, you can navigate to:
-- Model performance metrics (accuracy, Dice score, IoU)
-- Visual comparisons between ground truth and predictions
+- Model performance metrics (e.g., Dice score, IoU)
 - Dataset distribution and class analysis
 - Experimental results and observations
 - Testing Your MRI Scans
 
-This dashboard is designed to support **analysis, comparison, and interpretation** of different approaches in brain tumor detection and segmentation.            
+This dashboard is designed to support **analysis, comparison, and interpretation** of different approaches in brain tumor detection and segmentation.
+""")
+st.divider()
+st.markdown("""
+### Developed By
+**Weeraphat Oun-Ob**  
+**วีรภัทร์ อุ่นอบ**       
+Bachelor of Computer Engineering  
+
+### Project Advisor
+**Asst. Prof. Dr. Thanawin Rakthanmanon**  
+**ผู้ช่วยศาสตราจารย์ ดร.ธนาวินท์ รักธรรมานนท์**
+Department of Computer Engineering  
+---
 """)
